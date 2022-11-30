344fb5d9f3ecec5510950f33de48339e4b04c053
Joe DePumpo <joe@depumpo.com>
2022-11-30 16:24:14 +0000

    refactored modals and added delete to trips, edit still does not work

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -16,12 +16,23 @@ class TripsController < ApplicationController
   end
 
   def destroy
+    @trip = Trip.find(params[:id])
+    if @trip.destroy
+      redirect_to trips_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   def create
     @trip = Trip.new(trip_params)
     @trip.user = current_user
     @trip.save
+    if @trip.save
+      redirect_to trips_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   private
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,4 @@
 class Trip < ApplicationRecord
   belongs_to :user
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 end
