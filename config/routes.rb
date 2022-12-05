0975bd00c31d483e9bf68ac2d7fb3126fdb28bfd
Filipa Merino <filipaam@outlook.com>
2022-12-05 17:14:52 +0000

    login done - might need to change the picture

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   namespace :users do
     root to: "trips#index"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_152816) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_165553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_152816) do
     t.string "last_name"
     t.text "location"
     t.string "role"
+    t.string "provider"
+    t.string "uid"
+    t.string "facebook_picture_url"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
